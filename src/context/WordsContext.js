a080d47e64c61eadaf4770fd2807edece9c66fa2
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-20 19:16:45 +0300

    feat: Enhance WordProvider with editWord functionality
    Updated the WordProvider to include the editWord function,
    allowing for the modification of existing words in the context.
    The function takes updated data and the ID of the word to be
    edited, ensuring that the correct word is updated.

--- src/context/WordsContext.js
+++ src/context/WordsContext.js
@@ -6,20 +6,43 @@ const WordContext = createContext();
 export const WordProvider = ({ children }) => {
   const [words, setWords] = useState([]);
 
-  const addWord = (word) => {
+  const addWord = (newWordData) => {
+    if (!newWordData || Object.keys(newWordData).length === 0) {
+      console.error("Invalid data for adding a word");
+      return;
+    }
+
     const newWord = {
-      ...word,
+      ...newWordData,
       skor: 0,
       mistakeSkore: 0,
       correctSkore: 0,
       _id: uuidv4(),
     };
-    setWords([...words, newWord]);
+
+    setWords((prevWords) => [...prevWords, newWord]);
   };
 
-  const values = { words, setWords, addWord };
+  const editWord = (updatedData, id) => {
+    if (!updatedData || Object.keys(updatedData).length === 0) {
+      console.error("Invalid data for editing a word");
+      return;
+    }
 
-  return <WordContext.Provider value={values}>{children}</WordContext.Provider>;
+    setWords((prevWords) =>
+      prevWords.map((item) =>
+        item._id === id ? { ...item, ...updatedData } : item
+      )
+    );
+  };
+
+  const contextValues = { words, setWords, addWord, editWord };
+
+  return (
+    <WordContext.Provider value={contextValues}>
+      {children}
+    </WordContext.Provider>
+  );
 };
 
 export const useWords = () => useContext(WordContext);
