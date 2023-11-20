faa3f36952bf1fcf47e8b7096eafac6941643214
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-20 20:00:09 +0300

    feat: Add link to home page in Navbar component
    This commit adds a link to the home page in the Navbar
    component, allowing users to navigate back to the home page by
    clicking on the "Word Mentor" title.
    
    The following changes were made:
    - Added link to home page in Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Heading } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <Heading boxShadow="base" p="8" textAlign="center">
-      Word Mentor
+      <Link to="/">Word Mentor</Link>
     </Heading>
   );
 }
