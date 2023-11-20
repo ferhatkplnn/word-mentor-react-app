c2ca02fe228a13a2cef69e7e5abc76b0fe0124df
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-20 12:08:26 +0300

    feat: Create Navbar component
    Created a new Navbar component using Chakra UI with a Heading.

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+
+function Navbar() {
+  return (
+    <Heading boxShadow="base" p="8" textAlign="center">
+      Word Mentor
+    </Heading>
+  );
+}
+
+export default Navbar;
