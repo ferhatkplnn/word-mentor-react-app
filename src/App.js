3b71dc444c39650022f9e7623872fdf45af468b2
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-20 15:22:21 +0300

    refactor: Update App component structure
    Refactored the App component structure to accommodate routing. Removed hard-coded LinkCard elements and added the RouterElement component to handle different routes.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import LinkCard from "./components/LinkCard/LinkCard";
 import Navbar from "./components/Navbar/Navbar";
-import { Grid } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import RouterElement from "./router";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Grid templateColumns="repeat(3, 1fr)" mt="14" gap={10} mr="20%" ml="20%">
-        <LinkCard
-          title="Add a new word"
-          descreptionText="Add a new word to your word box or edit your words."
-        />
-        <LinkCard title="Beginner word box" />
-        <LinkCard title="Intermediate word box" />
-        <LinkCard title="Advanced word box" />
-        <LinkCard title="Random word box" />
-        <LinkCard title="Hard word box" />
-      </Grid>
+      <Box p="10">
+        <RouterElement />
+      </Box>
     </>
   );
 }
