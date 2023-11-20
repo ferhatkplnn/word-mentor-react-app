e7eac4d9d457617459e45adcc89016c411515172
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-20 12:20:43 +0300

    feat: Add LinkCard component to App
    Added LinkCard component to the App component to
    display different sections for word boxes. Also,
    updated the styling using Chakra UI's Grid component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import LinkCard from "./components/LinkCard/LinkCard";
+import Navbar from "./components/Navbar/Navbar";
+import { Grid } from "@chakra-ui/react";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Navbar />
+      <Grid templateColumns="repeat(3, 1fr)" mt="14" gap={10} mr="20%" ml="20%">
+        <LinkCard
+          title="Add a new word"
+          descreptionText="Add a new word to your word box or edit your words."
+        />
+        <LinkCard title="Beginner word box" />
+        <LinkCard title="Intermediate word box" />
+        <LinkCard title="Advanced word box" />
+        <LinkCard title="Random word box" />
+        <LinkCard title="Hard word box" />
+      </Grid>
+    </>
+  );
 }
 
 export default App;
