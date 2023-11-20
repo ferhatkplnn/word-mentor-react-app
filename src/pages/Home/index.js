e14d8477c11613464a59f845bd3c648ee536a082
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-20 15:14:37 +0300

    feat: Create Home page
    Created the Home page component with LinkCard elements for various word
    boxes.

---
+++ src/pages/Home/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Grid } from "@chakra-ui/react";
+import LinkCard from "../../components/LinkCard/LinkCard";
+
+function Home() {
+  return (
+    <Grid templateColumns="repeat(3, 1fr)" gap={10} mr="20%" ml="20%">
+      <LinkCard
+        title="Add a new word"
+        descreptionText="Add a new word to your word box or edit your words."
+        to="/addword"
+      />
+      <LinkCard title="Beginner word box" />
+      <LinkCard title="Intermediate word box" />
+      <LinkCard title="Advanced word box" />
+      <LinkCard title="Random word box" />
+      <LinkCard title="Hard word box" />
+    </Grid>
+  );
+}
+
+export default Home;
