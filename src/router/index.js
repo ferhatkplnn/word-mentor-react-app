90d7bd66aa9ef4e864b4854b6f6ac996858b96d7
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-21 02:57:08 +0300

    feat: Add new routes to RouterElement component
    This commit adds new routes to the RouterElement component, allowing
     navigation to the "BeginnerWordBox" and "IntermediateWordBox" pages.
    Users can now navigate to these new pages using the specified paths.
    
    The following changes were made:
    - Added new routes to RouterElement component
    - Imported BeginnerWordBox and IntermediateWordBox pages
    - Updated routeConfig to include paths for the new pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import { useRoutes } from "react-router-dom";
 import AddWord from "../pages/AddWord";
 import Home from "../pages/Home";
+import BeginnerWordBox from "../pages/BeginnerWordBox";
+import IntermediateWordBox from "../pages/IntermediateWordBox";
 
 function RouterElement() {
   const routeConfig = [
     { path: "/", element: <Home /> },
     { path: "/addword", element: <AddWord /> },
+    { path: "/beginner-word-box", element: <BeginnerWordBox /> },
+    { path: "/intermediate-word-box", element: <IntermediateWordBox /> },
   ];
 
   const routers = useRoutes(routeConfig);
