5545a1ec6f5050375e5fd2be36de0ce572e34a2e
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-20 15:17:17 +0300

    refactor: Wrap the app with WordProvider in index.js
    Wrapped the App component with the WordProvider to provide context related to
    words throughout the application.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { WordProvider } from "./context/WordsContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <WordProvider>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </WordProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
