c3ec0e3e8f82d6d40577ae353dcda33a135e4346
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-21 03:08:47 +0300

    feat: Add word box card component
    This commit adds the WordBoxCard component, which
    allows users to interact with words in a card format.
    The component includes functionality for displaying
    words, inputting meanings, and handling word scores.
    Users can navigate through words, input meanings,
    and see the correct meaning after submitting their input.
    
    The following changes were made:
    - Added WordBoxCard component for word interaction
    - Implemented word navigation and input functionality
    - Updated word score handling and display of correct meanings

---
+++ src/components/WordBoxCard/WordBoxCard.js
@@ -0,0 +1,113 @@
+import React, { useRef, useState } from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Stack,
+  FormControl,
+  Input,
+  Button,
+  ScaleFade,
+} from "@chakra-ui/react";
+import { useWords } from "../../context/WordsContext";
+
+function WordBoxCard({ title, limit }) {
+  const { getRandomWordId, getWord, increaseScore, decreaseScore } = useWords();
+  const [input, setInput] = useState("");
+  const [wordId, setWordId] = useState(
+    getRandomWordId(limit.minLimit, limit.maxLimit)
+  );
+
+  const [isDisabled, setIsDisabled] = useState(Boolean(!wordId));
+
+  const [isOpen, setIsOpen] = useState(false);
+  const inputRef = useRef(null);
+
+  const nextWord = () => {
+    setIsDisabled(false);
+    setIsOpen(false);
+    setTimeout(() => {
+      setWordId(getRandomWordId(limit.minLimit, limit.maxLimit));
+      inputRef.current.focus();
+    }, 300);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (input === getWord(wordId).meaning) {
+      setIsOpen(true);
+
+      increaseScore(wordId);
+
+      setInput("");
+      setIsDisabled(true);
+      setTimeout(nextWord, 1000);
+    } else {
+      decreaseScore(wordId);
+      setIsOpen(true);
+    }
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <>
+      <Heading textAlign="center" p="10" size="md">
+        {title}
+      </Heading>
+
+      <Card textAlign="center" maxW="40%" mx="auto">
+        <CardHeader>
+          <Heading size="lg">
+            {wordId ? getWord(wordId).word : "No more word"}
+          </Heading>
+        </CardHeader>
+
+        <CardBody>
+          <Stack spacing="6">
+            <ScaleFade in={isOpen}>
+              <Heading
+                textAlign="center"
+                p="30px"
+                fontSize="52px"
+                mt="4"
+                rounded="md"
+              >
+                {wordId ? getWord(wordId).meaning : ""}
+              </Heading>
+            </ScaleFade>
+
+            <form onSubmit={handleSubmit}>
+              <FormControl mb="2">
+                <Input
+                  type="text"
+                  name="word"
+                  placeholder="Meaning"
+                  required
+                  onChange={handleChange}
+                  value={input}
+                  disabled={isDisabled}
+                  ref={inputRef}
+                />
+              </FormControl>
+              <Button
+                width="full"
+                colorScheme="linkedin"
+                type="submit"
+                isDisabled={isDisabled}
+              >
+                Send
+              </Button>
+            </form>
+          </Stack>
+        </CardBody>
+      </Card>
+    </>
+  );
+}
+
+export default WordBoxCard;
