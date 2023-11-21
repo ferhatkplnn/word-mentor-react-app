024c06e137666437961649e7543cd5e71ff4129b
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-21 03:07:29 +0300

    feat: Add word management functionality
    This commit adds functionality for managing words,
    including adding, editing, and removing words from
    the word list. It also includes features for updating
    word scores, getting random word IDs, and retrieving
    specific words from the list.
    
    The following changes were made:
    - Added word management functionality
    - Updated word score handling
    - Implemented word retrieval and random word ID generation

--- src/context/WordsContext.js
+++ src/context/WordsContext.js
@@ -24,16 +24,16 @@ export const WordProvider = ({ children }) => {
 
     const newWord = {
       ...newWordData,
-      skor: 0,
-      mistakeSkore: 0,
-      correctSkore: 0,
+      score: 0,
+      mistakeScore: 0,
+      correctScore: 0,
       _id: uuidv4(),
     };
 
     setWords((prevWords) => [...prevWords, newWord]);
   };
 
-  const editWord = (updatedData, id) => {
+  const updateWord = (updatedData, id) => {
     if (!updatedData || Object.keys(updatedData).length === 0) {
       console.error("Invalid data for editing a word");
       return;
@@ -51,7 +51,68 @@ export const WordProvider = ({ children }) => {
     setWords(words.filter((item) => item._id !== id));
   };
 
-  const contextValues = { words, setWords, addWord, editWord, removeWord };
+  const getRandomWordId = (minLimit, maxLimit) => {
+    const filteredWords = words.filter(
+      (item) => item.score >= minLimit && item.score < maxLimit
+    );
+
+    if (filteredWords.length === 0) {
+      return;
+    }
+
+    let randomIndex = Math.floor(Math.random() * filteredWords.length);
+    let randomWord = filteredWords[randomIndex];
+
+    console.log(randomWord);
+    return randomWord._id;
+  };
+
+  const increaseScore = (id) => {
+    setWords(
+      words.map((item) => {
+        if (item._id === id) {
+          return {
+            ...item,
+            score: item.score + 1,
+            correctScore: item.correctScore + 1,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
+  const decreaseScore = (id) => {
+    setWords(
+      words.map((item) => {
+        if (item._id === id) {
+          return {
+            ...item,
+            score: item.score - 1,
+            mistakeScore: item.mistakeScore + 1,
+          };
+        }
+
+        return item;
+      })
+    );
+  };
+
+  const getWord = (id) => {
+    return words.find((word) => word._id === id);
+  };
+
+  const contextValues = {
+    words,
+    setWords,
+    addWord,
+    updateWord,
+    removeWord,
+    getRandomWordId,
+    getWord,
+    increaseScore,
+    decreaseScore,
+  };
 
   return (
     <WordContext.Provider value={contextValues}>
