4ffe6a680583fba5b3592351fb3c59a90c63f255
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-25 01:30:31 +0300

    feat: Add SentenceAddInputModel component
    This commit adds the SentenceAddInputModel component,
    which includes a modal for adding a new sentence related to a
    word. The modal allows the user to input a sentence and save it,
    or cancel the action.

---
+++ src/components/WordCardInput/SentenceAddInputModel.js
@@ -0,0 +1,62 @@
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  FormControl,
+  Input,
+  ModalFooter,
+} from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import { useSentence } from "../../context/SentenceContext";
+
+export default function SentenceAddInputModel({ isOpen, onClose, wordId }) {
+  const initialRef = useRef(null);
+  const { addSentence } = useSentence();
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addSentence(text, wordId);
+    setText("");
+    onClose();
+  };
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose} initialFocusRef={initialRef}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add sentence</ModalHeader>
+          <ModalCloseButton />
+
+          <form onSubmit={handleSubmit}>
+            <ModalBody pb={6}>
+              <FormControl>
+                <Input
+                  onChange={handleChange}
+                  value={text}
+                  ref={initialRef}
+                  placeholder="First name"
+                />
+              </FormControl>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} type="submit">
+                Save
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
