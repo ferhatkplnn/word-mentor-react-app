dbd52df694554d1a240d69bd9c865e918941eda3
Ferhat Kaplan <fkmll2121@gmail.com>
2023-11-29 16:30:15 +0300

    refactor: Enhance WordDisplay component with dynamic word rendering
    This commit refactors the WordDisplay component to improve code
    readability and maintainability. The displayWord variable is introduced
    using useMemo to conditionally render the original word or apply the
    hideRandomLetters function based on the isOpen prop. The
    component now dynamically renders the word with appropriate styles
    based on the isOpen prop.
    
    The following changes were made:
    - Refactored WordDisplay component for improved readability
    - Introduced displayWord variable for dynamic word rendering
    - Utilized useMemo for efficient memoization of displayWord
    - Conditionally rendered the original word or applied hideRandomLetters based on isOpen prop
    - Adjusted styling for consistent presentation

--- src/components/WordBoxCard/WordDisplay/WordDisplay.js
+++ src/components/WordBoxCard/WordDisplay/WordDisplay.js
@@ -1,11 +1,41 @@
-import { Heading, ScaleFade } from "@chakra-ui/react";
+import React, { useMemo } from "react";
+import { Box, Heading } from "@chakra-ui/react";
+import hideRandomLetters from "../../../utils/hideRandomLetters";
 
-const WordDisplay = ({ isOpen, wordId, word }) => (
-  <ScaleFade in={isOpen}>
-    <Heading textAlign="center" p="30px" fontSize="52px" mt="4" rounded="md">
-      {wordId ? word : ""}
-    </Heading>
-  </ScaleFade>
-);
+const WordDisplay = ({ isOpen, wordId, word }) => {
+  const displayWord = useMemo(
+    () => (isOpen ? word : hideRandomLetters(word)),
+    [isOpen, word]
+  );
+
+  return (
+    <Box minH="30vh">
+      {isOpen && (
+        <Heading
+          fontFamily="monospace"
+          textAlign="center"
+          p="30px"
+          fontSize="52px"
+          mt="4"
+          rounded="md"
+        >
+          {wordId && displayWord}
+        </Heading>
+      )}
+      {!isOpen && (
+        <Heading
+          fontFamily="monospace"
+          textAlign="center"
+          p="30px"
+          fontSize="52px"
+          mt="4"
+          rounded="md"
+        >
+          {displayWord}
+        </Heading>
+      )}
+    </Box>
+  );
+};
 
 export default WordDisplay;
